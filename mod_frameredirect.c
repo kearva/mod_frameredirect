0a6d8d18b67b0a4af365e9abb4f5322b20da75ed
Jan Ingvoldstad <frettled@gmail.com>
2013-12-05 16:12:06 +0100

    Copyright years fixed

--- mod_frameredirect.c
+++ mod_frameredirect.c
@@ -1,5 +1,5 @@
 /*
-   Copyright 2011, 2012 Kent Are Varmedal, Jan Ingvoldstad
+   Copyright 2011-2013 Kent Are Varmedal, Jan Ingvoldstad
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
